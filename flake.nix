227bd244f6f61935d9eee79be7a04f6ce01eba45
Gerwin van der Lugt <account+git@gerwin3.com>
2025-01-13 16:50:13 +0100

    flake.nix can be greatly simplified now that we are doing full dynamic dlopen

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "openh264";
+  description = "nvidia-video-codec-sdk";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -34,30 +34,7 @@
           packages = with pkgs; [
             zigpkgs.default
             zls
-            cudaPackages.cudatoolkit
           ];
-
-          # The shell hook makes sure that:
-          # * `CUDA_PATH` points to the location of `libcudart` and friends.
-          #   `CUDA_PATH` is the de facto standard for pointing build scripts to
-          #   the CUDA library directory.
-          # * If the host is NixOS, we arrange for `libcuda.so` to be linked
-          #   correctly by using `LD_LIBRARY_PATH` and `EXTRA_LDFLAGS`. The
-          #   preferred way to do it would be to link to the stubs and then use
-          #   rpath later (and this is what we'll do when packaging) but for
-          #   development it is easier to just directly link to the `nvidia_x11`
-          #   package dir, which is assumed to be installed.
-          # * Issues a warning for non-NixOS systems.
-          shellHook = ''
-            export CUDA_PATH=${pkgs.cudaPackages.cuda_cudart}
-            if [ -f "/etc/NIXOS" ]; then
-              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.linuxPackages.nvidia_x11}/lib
-              export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-            else
-              echo "warning: no support for linking libcuda on non-NixOS"
-              echo " \`-> refer to https://nixos.wiki/wiki/Nixpkgs_with_OpenGL_on_non-NixOS for more information"
-            fi
-          '';
         };
       }
     );
