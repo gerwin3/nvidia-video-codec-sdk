3d12ca68c36d1b0b43e40dc8a98840de3d50ceb2
Gerwin van der Lugt <account+git@gerwin3.com>
2025-01-13 14:44:18 +0100

    cleaned up

--- build.zig
+++ build.zig
@@ -27,6 +27,13 @@ pub const Version = enum {
     }
 };
 
+// TODO: There are two strats for linking:
+// - 1. Normal linking assuming the user has everything setup correctly. (this is what we do now)
+// - 2. runtime linking where we link against stubs and then use rpath on the target system:
+// - 3. linking with the PyPi packages similar to what PyTorch does presently,
+//      this is the same as (2) but then we use specific rpaths (see PyTorch CMake config)
+// We want this to be configurable later
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -37,8 +44,21 @@ pub fn build(b: *std.Build) !void {
         "Use headers of specified NVIDIA Video Codec SDK version. Note that this does not impact what library verison is loaded since they are loaded dynamically.",
     ) orelse .@"12.2";
 
-    const nvidia_video_codec_sdk = b.addStaticLibrary(.{
-        .name = "nvidia_video_codec_sdk",
+    const cuda_paths = try getCudaPaths(b);
+
+    addDecodingLibraryAndBindings(build, target, optimize, version, &cuda_paths);
+    addEncodingLibraryAndBindings(build, target, optimize, version, &cuda_paths);
+}
+
+fn addDecodingLibraryAndBindings(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    version: Version,
+    cuda_paths: *const CudaPaths,
+) void {
+    const lib_nvdec = b.addStaticLibrary(.{
+        .name = "nvdec",
         .target = target,
         .optimize = optimize,
         // Zig will fail to produce a library without any source code since the linker needs
@@ -46,54 +66,97 @@ pub fn build(b: *std.Build) !void {
         .root_source_file = b.addWriteFiles().add("empty.zig", ""),
     });
 
-    const cuda_root_dir = try getCudaRootDir(b.allocator);
-    const cuda_include_dir = try std.fs.path.join(b.allocator, &.{ cuda_root_dir, "include" });
-    const cuda_h = try std.fs.path.join(b.allocator, &.{ cuda_include_dir, "cuda.h" });
-    const cuda_lib_dir = try std.fs.path.join(b.allocator, &.{ cuda_root_dir, "lib" });
-
-    nvidia_video_codec_sdk.addIncludePath(.{ .cwd_relative = cuda_include_dir });
-    nvidia_video_codec_sdk.addLibraryPath(.{ .cwd_relative = cuda_lib_dir });
-
-    nvidia_video_codec_sdk.linkLibC();
-
-    // TODO: There are two strats for linking:
-    // - 1. Normal linking assuming the user has everything setup correctly.
-    nvidia_video_codec_sdk.linkSystemLibrary("cuda");
-    nvidia_video_codec_sdk.linkSystemLibrary("cudart");
-    nvidia_video_codec_sdk.linkSystemLibrary("nvcuvid");
-    nvidia_video_codec_sdk.linkSystemLibrary("nvidia-encode");
-    // TODO: - 2. runtime linking where we link against stubs and then use rpath on the target system:
-    // TODO: - 3. linking with the PyPi packages similar to what PyTorch does presently,
-    //            this is the same as (2) but then we use specific rpaths (see PyTorch CMake config)
-
-    nvidia_video_codec_sdk.installHeadersDirectory(b.path(version.getIncludeDir()), "", .{});
+    lib_nvdec.addIncludePath(.{ .cwd_relative = cuda_paths.include_dir });
+    lib_nvdec.addLibraryPath(.{ .cwd_relative = cuda_paths.lib_dir });
+    lib_nvdec.linkLibC();
+    lib_nvdec.linkSystemLibrary("cuda");
+    lib_nvdec.linkSystemLibrary("cudart");
+    lib_nvdec.linkSystemLibrary("nvcuvid");
+    lib_nvdec.installHeader(b.path(version.getIncludeDir()).path("cuviddec.h"), "cuviddec.h");
+    lib_nvdec.installHeader(b.path(version.getIncludeDir()).path("nvcuvid.h"), "nvcuvid.h");
     // This is a little hacky since we are copying out cuda.h and adding it to the
     // install headers of this library without it even being part of this repo. But
     // without it the SDK headers could not be used since they require cuda.h.
-    nvidia_video_codec_sdk.installHeader(.{ .cwd_relative = cuda_h }, "cuda.h");
+    lib_nvdec.installHeader(.{ .cwd_relative = cuda_paths.cuda_h }, "cuda.h");
 
-    // NOTE: We switch terminology here and refer to the entire SDK as "nvcodec".
-    // This is also the name we use to refer to the Zig library in general.
+    b.installArtifact(lib_nvdec);
 
-    // Bindings
-
-    const nvcodec_bindings = b.addModule("nvcodec_bindings", .{
-        .root_source_file = b.path("nvcodec_bindings.zig"),
+    // Raw bindings
+    const nvdec_bindings = b.addModule("nvdec_bindings", .{
+        .root_source_file = b.path("nvdec_bindings.zig"),
         .target = target,
         .optimize = optimize,
     });
-    nvcodec_bindings.linkLibrary(nvidia_video_codec_sdk);
+    nvdec_bindings.linkLibrary(lib_nvdec);
 
     // Zig-friendly API
-
-    const nvcodec = b.addModule("nvcodec", .{
-        .root_source_file = b.path("nvcodec.zig"),
+    const nvdec = b.addModule("nvdec", .{
+        .root_source_file = b.path("nvdec.zig"),
         .target = target,
         .optimize = optimize,
     });
-    nvcodec.addImport("nvcodec_bindings", nvcodec_bindings);
+    nvdec.addImport("nvdec_bindings", nvdec_bindings);
+}
 
-    b.installArtifact(nvidia_video_codec_sdk);
+fn addEncodingLibraryAndBindings(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    version: Version,
+    cuda_paths: *const CudaPaths,
+) void {
+    const lib_nvenc = b.addStaticLibrary(.{
+        .name = "nvenc",
+        .target = target,
+        .optimize = optimize,
+        // Zig will fail to produce a library without any source code since the linker needs
+        // something to link, so we include an empty source file.
+        .root_source_file = b.addWriteFiles().add("empty.zig", ""),
+    });
+
+    lib_nvenc.addIncludePath(.{ .cwd_relative = cuda_paths.include_dir });
+    lib_nvenc.addLibraryPath(.{ .cwd_relative = cuda_paths.lib_dir });
+    lib_nvenc.linkLibC();
+    lib_nvenc.linkSystemLibrary("cuda");
+    lib_nvenc.linkSystemLibrary("cudart");
+    lib_nvenc.linkSystemLibrary("nvidia-encode");
+    lib_nvenc.installHeader(b.path(version.getIncludeDir()).path("nvEncodeAPI.h"), "nvEncodeAPI.h");
+
+    // Raw bindings
+    const nvenc_bindings = b.addModule("nvenc_bindings", .{
+        .root_source_file = b.path("nvenc_bindings.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    nvenc_bindings.linkLibrary(lib_nvenc);
+
+    // Zig-friendly API
+    const nvenc = b.addModule("nvenc", .{
+        .root_source_file = b.path("nvenc.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    nvenc.addImport("nvenc_bindings", nvenc_bindings);
+
+    b.installArtifact(lib_nvenc);
+}
+
+const CudaPaths = struct {
+    root_dir: []const u8,
+    include_dir: []const u8,
+    cuda_h: []const u8,
+    lib_dir: []const u8,
+};
+
+fn getCudaPaths(b: *std.Build) !CudaPaths {
+    const root_dir = try getCudaRootDir(b.allocator);
+    const include_dir = try std.fs.path.join(b.allocator, &.{ root_dir, "include" });
+    return .{
+        .root_dir = root_dir,
+        .include_dir = include_dir,
+        .cuda_h = try std.fs.path.join(b.allocator, &.{ include_dir, "cuda.h" }),
+        .lib_dir = try std.fs.path.join(b.allocator, &.{ root_dir, "lib" }),
+    };
 }
 
 fn getCudaRootDir(allocator: std.mem.Allocator) ![]const u8 {
